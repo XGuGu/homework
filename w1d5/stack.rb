031aaa06e350aacb148de9c03abbf3a639f963c8
lingjie zhang <zhanglingjie16303@gmail.com>
2018-07-26 22:08:10 -0400

    finish stack

---
+++ w1d5/stack.rb
@@ -0,0 +1,20 @@
+class Stack
+  def initialize
+    @stack = []
+    @size = 0
+  end
+
+  def add(num)
+    @stack << num
+    @size += 1
+  end
+
+  def remove
+    @size -= 1
+    @stack.pop
+  end
+
+  def show
+    @stack.dup
+  end
+end
