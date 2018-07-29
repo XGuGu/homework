2e9983ee3d0e0d98315c64f1de22fd599a81998f
lingjie zhang <zhanglingjie16303@gmail.com>
2018-07-29 10:42:40 -0400

    finish homework Simon

---
+++ w2d1/skeleton/lib/simon.rb
@@ -0,0 +1,54 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @seq = []
+    @game_over = false
+    # @seq.push(COLORS.sample)
+  end
+
+  def play
+    until @game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
+  end
+
+  def take_turn
+    show_sequence
+    require_sequence
+    round_success_message
+    @sequence_length += 1
+  end
+
+  def show_sequence
+    add_random_color
+  end
+
+  def require_sequence
+
+  end
+
+  def add_random_color
+    @seq.push(COLORS.sample)
+  end
+
+  def round_success_message
+
+  end
+
+  def game_over_message
+    puts"game over"
+    p @seq 
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+end
