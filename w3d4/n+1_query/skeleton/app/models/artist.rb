7d704103de6a5b31fb5462fb703c195b0b11271d
lingjie zhang <zhanglingjie16303@gmail.com>
2018-08-09 00:24:43 -0400

    Finish homework w3d4

---
+++ w3d4/n+1_query/skeleton/app/models/artist.rb
@@ -0,0 +1,31 @@
+class Artist < ApplicationRecord
+  has_many :albums,
+    class_name: 'Album',
+    foreign_key: :artist_id,
+    primary_key: :id
+
+  def n_plus_one_tracks
+    albums = self.albums
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.title] = album.tracks.length
+    end
+
+    tracks_count
+  end
+
+  def better_tracks_query
+    # TODO: your code here
+    albums = self
+    .albums
+      .select('albums.*, count(*) as num_tracks')
+      .joins(:tracks)
+      .group('albums.id')
+
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.title] = album.num_tracks
+    end
+    tracks_count
+  end
+end
